e08d08e0a68b73fee813ab0be4f9020495c14df5
Miodini <lucasmondini01@gmail.com>
2025-09-28 15:18:53 -0300

    Changing GH link from image to anchor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,15 @@
-'use client'
 import React from 'react'
 import style from '@/app/main.module.css'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
 import GithubMark from '@/../public/github-mark.svg'
 import buildInfo from '@/../.buildinfo.json'
 
 export default function Footer() {
-  const router = useRouter()
-  const navigateToGithub = () => {
-    router.push('https://github.com/Miodini/Color-Playground')
-  }
-
   return (
     <footer className={style.footer}>
-      <Image src={GithubMark} alt="Github" className={style.githubIcon} onClick={navigateToGithub}/>
+      <a href="https://github.com/Miodini/Color-Playground" className={style.githubLink}>
+        <Image src={GithubMark} alt="GitHub" className={style.githubIcon} />
+      </a>
       <span>
         {new Date(buildInfo?.buildDate).getFullYear()}
       </span>
