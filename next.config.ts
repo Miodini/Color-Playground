52dfef152bada3bc0992d6b426ba7043e7785d9e
Miodini <lucasmondini01@gmail.com>
2025-09-28 15:19:08 -0300

    Fixing production build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/color-playground/',
-  basePath: process.env.NODE_ENV === 'development' ? '' : '/color-playground/',
+  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/color-playground',
+  basePath: process.env.NODE_ENV === 'development' ? '' : '/color-playground',
   output: 'export'
 };
 
